06ff93b3e01988d419c23112778daad23caeaf91
RoccoDev <roccodev.business@gmail.com>
2019-02-28 18:23:16 +0100

    Schematics prototype

--- src/main/java/pw/roccodev/bukkit/practice/utils/schema/SchematicUtils.java
+++ src/main/java/pw/roccodev/bukkit/practice/utils/schema/SchematicUtils.java
@@ -1,27 +1,18 @@
 package pw.roccodev.bukkit.practice.utils.schema;
 
-import com.google.common.io.Closer;
+import com.sk89q.worldedit.CuboidClipboard;
 import com.sk89q.worldedit.EditSession;
-import com.sk89q.worldedit.LocalSession;
-import com.sk89q.worldedit.MaxChangedBlocksException;
 import com.sk89q.worldedit.Vector;
+import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.blocks.BaseBlock;
 import com.sk89q.worldedit.bukkit.BukkitWorld;
-import com.sk89q.worldedit.extent.clipboard.Clipboard;
-import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
-import com.sk89q.worldedit.extent.clipboard.io.ClipboardReader;
-import com.sk89q.worldedit.function.operation.Operation;
-import com.sk89q.worldedit.function.operation.Operations;
-import com.sk89q.worldedit.session.ClipboardHolder;
-import com.sk89q.worldedit.world.registry.WorldData;
+import com.sk89q.worldedit.schematic.SchematicFormat;
 import org.bukkit.Location;
-import org.bukkit.World;
+import org.bukkit.Material;
 import pw.roccodev.bukkit.practice.PracticePlugin;
 import pw.roccodev.bukkit.practice.arena.ArenaMap;
 
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 
 /*
 TODO Complete this class
@@ -30,68 +21,48 @@ TODO Complete this class
 
 public class SchematicUtils {
 
-    public static void pasteSchematic(String name, Location result, World world) {
+    public static void pasteSchematic(String name, Location result) {
+        File f = getSchematic(name);
+
+        Vector vec = new Vector(result.getBlockX(), result.getBlockY(), result.getBlockZ());
+
+        EditSession es = WorldEdit.getInstance().getEditSessionFactory().getEditSession(
+                new BukkitWorld(result.getWorld()), WorldEdit.getInstance().getConfiguration().maxChangeLimit);
+
+        SchematicFormat format = SchematicFormat.getFormat(f);
         try {
-            File f = getSchematic(name);
-
-            LocalSession editSession = new LocalSession();
-
-            ClipboardFormat format = ClipboardFormat.findByFile(f);
-
-            try (Closer closer = Closer.create()) {
-                FileInputStream fis = closer.register(new FileInputStream(f));
-                BufferedInputStream bis = closer.register(new BufferedInputStream(fis));
-                ClipboardReader reader = format.getReader(bis);
-
-                WorldData worldData = new BukkitWorld(world).getWorldData();
-                Clipboard clipboard = reader.read(worldData);
-                ClipboardHolder holder = new ClipboardHolder(clipboard, worldData);
-                editSession.setClipboard(holder);
-
-                EditSession session = new EditSession(new BukkitWorld(world), 999999999);
-                session.enableQueue();
-
-
-                Vector to = new Vector(result.getX(), result.getY(), result.getZ());
-                Operation operation = holder
-                        .createPaste(session, worldData)
-                        .to(to)
-                        .ignoreAirBlocks(false)
-                        .build();
-                Operations.completeLegacy(operation);
-                session.flushQueue();
-
-            } catch (IOException e) {
-               e.printStackTrace();
-            }
-
-
-
-        } catch (MaxChangedBlocksException ex) {
+            CuboidClipboard cc = format.load(f);
+            cc.paste(es, vec, false);
+            generateSpawnPoints(null, cc, vec);
+        }
+        catch (Exception ex) {
             ex.printStackTrace();
         }
+
+
     }
 
     private static File getSchematic(String name) {
         return new File(PracticePlugin.PLUGIN_DIR + "/maps/schematics/" + name);
     }
 
-    public static void generateSpawnPoints(ArenaMap map, Clipboard clip) {
+    public static void generateSpawnPoints(ArenaMap map, CuboidClipboard clip, Vector vec) {
 
-        /*
-            if(bl.getID().getId() == BlockID.SIGN_POST) {
-                CompoundTag nbt = bl.getID().getNbtData();
-                if(nbt == null) continue;
-                if(!nbt.containsKey("Text1")) continue;
+        for (int x = 0; x < vec.getBlockX(); ++x) {
+            for (int y = 0; y < vec.getBlockY(); ++y) {
+                for (int z = 0; z < vec.getBlockZ(); ++z) {
+                    final BaseBlock block = clip.getBlock(new Vector(x, y, z));
+                    if (block == null) {
+                        continue;
+                    }
 
-                String firstLine = nbt.getString("Text1");
-                if(firstLine.equals("[SpawnPoint]")) {
-
+                   if(block.getId() == Material.SIGN_POST.getId()) {
+                       System.out.println("Found sign at " + x + " " + y + " " + z);
+                   }
                 }
-
             }
         }
-        */
+
     }
 
 }
